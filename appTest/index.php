22c7c96b2d7228f69dfe452828fdc39e92eaad72
Zak-65 <sidqui.zakaria1@gmail.com>
2024-04-15 21:09:43 +0100

    echo pho

--- appTest/index.php
+++ appTest/index.php
@@ -1,14 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Learn PHP</title>
+    <link rel="stylesheet" href="./src/output.css">
+    <title>learn php</title>
 </head>
+
 <body>
-    <h1>
-        Learn PHP
-    </h1>
-    <?php echo"Hello world"; ?>
+    <?php $prenom = "zakaria";
+    $nom = "sidqui";
+    $age = 22;
+    $isStudent = true;
+    $note = 19;
+     ?>
+    <header class="flex justify-center items-center h-32 bg-blue-500">
+        <div class="text-white">M107 : Dévlopper de sites web dynamiques</div>
+    </header>
+    <?php echo "<h1 class=\"text-red-300 text-3xl text-center\">This is php text</h1>"; ?>
+    <main class="container mx-auto px-32 w-full">
+        <div class="h-64 border border-gray-200 rounded-md bg-white">
+            I am <?php echo $prenom.' '.$nom;?>,and I am a Copmuter developer trainee !
+            <br/>
+            <br/>
+            <?php echo "I am {$prenom} {$nom} ,and I am a Copmuter developer trainee !"?>
+        </div>
+    </main>
 </body>
+
 </html>
