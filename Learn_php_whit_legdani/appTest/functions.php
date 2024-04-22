babcbc723af645f3d7876781823ecb7f3b6a31cb
Zak-65 <sidqui.zakaria1@gmail.com>
2024-04-22 17:41:09 +0100

    cours 22/04/2024

---
+++ Learn_php_whit_legdani/appTest/functions.php
@@ -0,0 +1,6 @@
+<?php
+function sayHello(){
+    echo 'hello dear';
+};
+
+sayHello();
---
+++ Learn_php_whit_legdani/appTest/varibles.php
@@ -0,0 +1,11 @@
+<?php
+// 1st methode to declar cosnt
+    echo "<br/>";
+    define('DB_NAME','first const');
+    echo DB_NAME;
+// 2nd methode to declar cosnt
+    echo "<br/>";
+    const PI = 3.14;
+    echo PI;
+    echo "<br/>";
+    
