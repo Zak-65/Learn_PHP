489137b651f3c8f3f9a745772e015dee6b333d28
Zak-65 <sidqui.zakaria1@gmail.com>
2024-04-24 21:55:41 +0100

    if statment

---
+++ Learn_php_whit_legdani/appTest/conditions.php
@@ -0,0 +1,11 @@
+<?php 
+// condition if else in php
+    $t = 0.5 ;
+    if($t > 0.6){
+        echo "true";
+    }elseif($t<1){
+        echo "hello";
+    }
+    else{
+        echo "false";
+    };
